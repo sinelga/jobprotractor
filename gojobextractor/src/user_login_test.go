454147e45dc2fc1eb88253b638b5a2330f8ca5f7
juno <juno@krasava.fi>
2016-04-29 03:40:15 +0300

    vdev

--- gojobextractor/src/user_login_test.go
+++ gojobextractor/src/user_login_test.go
@@ -1,107 +1,74 @@
 package src_test
 
-
-
+//
 import (
-    "testing"
-//    "fmt"
-   
-    "github.com/sclevine/agouti"
-    am "github.com/sclevine/agouti/matchers"
-    gm "github.com/onsi/gomega"
+	"fmt"
+	"os"
+	"testing"
+	//   "time"
+	gm "github.com/onsi/gomega"
+	"github.com/sclevine/agouti"
+	am "github.com/sclevine/agouti/matchers"
 )
 
 func TestUserLoginPrompt(t *testing.T) {
-    gm.RegisterTestingT(t)
+	gm.RegisterTestingT(t)
 
-    driver := agouti.ChromeDriver()
-    gm.Expect(driver.Start()).To(gm.Succeed())
-    page, err := driver.NewPage(agouti.Browser("chrome"))
-    gm.Expect(err).NotTo(gm.HaveOccurred())
+	email := os.Getenv("login")
+	pass := os.Getenv("pass")
 
-//    potato.StartMyApp(3000)
+	gm.Expect(email).Should(gm.HaveLen(18))
 
-    gm.Expect(page.Navigate("http://careers.stackoverflow.com")).To(gm.Succeed())
-    gm.Expect(page).To(am.HaveURL("http://careers.stackoverflow.com/"))
-    
-//   fmt.Println(page.Title())
-	gm.Expect(page.FindByID("content")).To(gm.Succeed())
-//     (page.FindByID("content").To(gm.Succeed())
-//    gm.Expect(page.Find("#prompt")).To(am.HaveText("Please login!"))
-//	gm.Expect(page.Title()).To(am.HaveText("Please login!"))
+	driver := agouti.ChromeDriver()
+	gm.Expect(driver.Start()).To(gm.Succeed())
+	page, err := driver.NewPage(agouti.Browser("chrome"))
+	gm.Expect(err).NotTo(gm.HaveOccurred())
 
-    gm.Expect(driver.Stop()).To(gm.Succeed()) // calls page.Destroy() automatically
+	gm.Expect(page.Navigate("https://stackoverflow.com/users/login?ssrc=head&returnurl=http%3a%2f%2fstackoverflow.com%2fjobs")).To(gm.Succeed())
+	gm.Expect(page).To(am.HaveURL("https://stackoverflow.com/users/login?ssrc=head&returnurl=http%3a%2f%2fstackoverflow.com%2fjobs"))
+
+	gm.Eventually(page.FindByID("email")).Should(am.BeFound())
+	gm.Expect(page.FindByID("email").Fill(email)).To(gm.Succeed())
+	gm.Eventually(page.FindByID("password")).Should(am.BeFound())
+	gm.Expect(page.FindByID("password").Fill(pass)).To(gm.Succeed())
+	gm.Eventually(page.FindByID("submit-button")).Should(am.BeFound())
+	gm.Expect(page.FindByID("submit-button").Submit()).To(gm.Succeed())
+	gm.Expect(page).To(am.HaveURL("http://stackoverflow.com/jobs"))
+
+	listResults := page.AllByClass("listResults")
+	gm.Expect(listResults.At(1).AllByClass("-item")).Should(am.BeFound())
+	items := listResults.At(1).AllByClass("-item")
+	gm.Expect(items.Count()).Should(gm.Equal(25))
+	for i := 0; i < 25; i++ {
+		
+		gm.Expect(items.At(i).FindByClass("-title")).Should(am.BeFound())
+		
+		title :=items.At(i).FindByClass("-title")
+		
+		gm.Expect(title.FindByClass("job-link")).Should(am.BeFound())
+		job_link :=title.FindByClass("job-link")
+		
+		fmt.Println(job_link.Attribute("href")) 
+//		gm.Expect(job_link).Should(am.HaveAttribute("title","title"))
+		
+//		gm.Expect(job_link).Should()	
+				
+//		agouti.
+
+	}
+	//		fmt.Println("ls",)
+	gm.Expect(driver.Stop()).To(gm.Succeed()) // calls page.Destroy() automatically
 }
 
-
-
-
-
 //import (
-//    
-//    . "github.com/onsi/ginkgo"
-//    . "github.com/onsi/gomega"
-////    . "github.com/sclevine/agouti/matchers"
-//    "github.com/sclevine/agouti"
+//
+//	. "github.com/onsi/ginkgo"
+//	. "github.com/onsi/gomega"
+//	"github.com/sclevine/agouti"
+//	. "github.com/sclevine/agouti/matchers"
 //)
 //
-//var _ = Describe("UserLogin", func() {
-//    var page *agouti.Page
 //
-//    BeforeEach(func() {
-//        var err error
-//        page, err = agoutiDriver.NewPage()
-//        Expect(err).NotTo(HaveOccurred())
-//    })
-//
-//    AfterEach(func() {
-//        Expect(page.Destroy()).To(Succeed())
-//    })
-//})
-
-
-
-
-
-
-
-
-
-//import (
-//	
-//	"testing"
-//    
-//    "github.com/sclevine/agouti"
-//    am "github.com/sclevine/agouti/matchers"
-//    gm "github.com/onsi/gomega"	
-////	. "github.com/onsi/ginkgo"
-////	. "github.com/onsi/gomega"
-////	"github.com/sclevine/agouti"
-////	. "github.com/sclevine/agouti/matchers"
-//)
-
-
-//func TestUserLoginPrompt(t *testing.T) {
-//	
-//	gm.RegisterTestingT(t)
-//
-//    driver := agouti.Selenium()
-//    gm.Expect(driver.Start()).To(gm.Succeed())
-//    page, err := driver.NewPage(agouti.Browser("chrome"))
-//    gm.Expect(err).NotTo(gm.HaveOccurred())
-//
-////    potato.StartMyApp(3000)
-//
-//    gm.Expect(page.Navigate("http://localhost:3000")).To(gm.Succeed())
-//    gm.Expect(page).To(am.HaveURL("http://localhost:3000"))
-//    gm.Expect(page.Find("#prompt")).To(am.HaveText("Please login!"))
-//
-//    gm.Expect(driver.Stop()).To(gm.Succeed()) // calls page.Destroy() automatically	
-//	
-//	
-//}
-
-
 //var _ = Describe("UserLogin", func() {
 //	var page *agouti.Page
 //
@@ -124,8 +91,24 @@ func TestUserLoginPrompt(t *testing.T) {
 //		})
 //
 //		By("allowing the user to fill out the login form and submit it", func() {
-//				
-//				
+//			Eventually(page.FindByID("email")).Should(BeFound())
+//			Expect(page.FindByID("email").Fill("support@mazurov.eu")).To(Succeed())
+//			Eventually(page.FindByID("password")).Should(BeFound())
+//			Expect(page.FindByID("password").Fill("stackexchange1Rel")).To(Succeed())
+//			Eventually(page.FindByID("submit-button")).Should(BeFound())
+//			Expect(page.FindByID("submit-button").Submit()).To(Succeed())
+//
+//		})
+//
+//		By("get_all_job_links", func() {
+//
+//			Expect(page).To(HaveURL("https://stackoverflow.com/jobs"))
+//			Expect(page.AllByClass("listResults")).Should(BeFound())
+//			listResults :=page.AllByClass("listResults")
+//			Expect(listResults.At(1).AllByClass("-item")).Should(BeFound())
+//			items :=listResults.At(1).AllByClass("-item")
+//			Expect(items.Count()).Should(Equal(25))
+//
 //		})
 //
 //	})
--- gojobextractor/usefull.bash
+++ gojobextractor/usefull.bash
@@ -1,6 +1,7 @@
 export PATH=$PATH:/home/juno/selenium
 
-/home/juno/workspace/gocode/bin/ginkgo bootstrap --agouti
+
+$GOPATH/bin/ginkgo bootstrap --agouti
 
 GOPATH=$GOPATH:/home/juno/git/jobprotractor/gojobextractor /home/juno/workspace/gocode/bin/ginkgo generate --agouti user_login
 
